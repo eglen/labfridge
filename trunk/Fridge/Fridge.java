53f90af4c7868bd59de2e3666b47fbc94bc0ea3b
edward.glen <edward.glen@90a643c4-8f1e-0410-8fdb-9fd79a773019>
2006-10-02 06:50:55 +0000

    Added the ability to actually quit when you hit the close button on the window

--- trunk/Fridge/Fridge.java
+++ trunk/Fridge/Fridge.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -83,6 +85,16 @@ public class Fridge extends javax.swing.JFrame implements ActionListener, KeyLis
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		
+//		Ability to quit
+	    addWindowListener(new WindowAdapter()
+	    {
+	      public void windowClosing(WindowEvent e)
+	      {
+	        System.out.println("Fridge Closing");
+	        System.exit(0);
+	      }
+	    });
 	}
 
 	private Statement getDbStatement() throws SQLException {
